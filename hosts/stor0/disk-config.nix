f958255f846b35afaab62692b7000c6333355094
John W. Terrell <john@alphapixel.com>
2025-10-25 18:22:04 -0700

    Added first ZFS config to stor0

--- hosts/stor0/disk-config.nix
+++ hosts/stor0/disk-config.nix
@@ -2,27 +2,77 @@
 { lib, ... }:
 {
   disko.devices = {
-    disk.disk1 = {
-      device = lib.mkDefault "/dev/disk/by-id/ata-Lexar_256GB_SSD_MBN625W106329";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          esp = {
-            size = "500M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions =  [ "umask=0077" ];
+    disk = {
+      disk1 = {
+        device = lib.mkDefault "/dev/disk/by-id/ata-Lexar_256GB_SSD_MBN625W106329";
+        type = "disk";
+        content = {
+            type = "gpt";
+            partitions = {
+            esp = {
+                size = "500M";
+                type = "EF00";
+                content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions =  [ "umask=0077" ];
+                };
+            };
+            zfs = {
+                size = "100%";
+                content = {
+                type = "zfs";
+                pool = "rpool";
+                };
+            };
+            };
+        };
+      };
+      array_11 = {
+        type = "disk";
+        device = "/dev/disk/by-id/ata-HGST_HDN724040ALE640_PK1334PEKDVLBS";
+        content = {
+          type = "gpt";
+          partitions = {
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "stor";
+              };
             };
           };
-          zfs = {
-            size = "100%";
-            content = {
-              type = "zfs";
-              pool = "rpool";
+        };
+      };
+      array_12 = {
+        type = "disk";
+        device = "/dev/disk/by-id/ata-HGST_HDN724040ALE640_PK2338P4HANSYC";
+        content = {
+          type = "gpt";
+          partitions = {
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "stor";
+              };
+            };
+          };
+        };
+      };
+      array_13 = {
+        type = "disk";
+        device = "/dev/disk/by-id/ata-HGST_HDN724040ALE640_PK2334PCG4HVXB";
+        content = {
+          type = "gpt";
+          partitions = {
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "stor";
+              };
             };
           };
         };
@@ -67,6 +117,27 @@
             options.mountpoint = "legacy";
             mountpoint = "/persist";
           };
+        };
+      };
+      stor = {
+        type = "zpool";
+        options.cacheFile = "none";
+        mode = {
+          topology = {
+            type = "topology";
+            vdev = [
+              mode = "mirror";
+              members = [
+                "array_11"
+                "array_12"
+                "array_13"
+              ]
+            ];
+          };
+        };
+        rootFsOptions = {
+          compression = "zstd";
+          "com.sun:auto-snapshot" = "false";
         };
       };
     };
